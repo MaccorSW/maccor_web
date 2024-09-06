ae76ef65c53c269248d7576c89e2f52f6fe2d1dd
MikeSando <34343247+MikeSando@users.noreply.github.com>
2024-09-06 14:22:14 -0500

    remove formsubmit.io

--- contactForm.php
+++ contactForm.php
@@ -2,14 +2,14 @@
 
 if (isset($_POST['submit'])) {
 $name = $_POST['name'];
-$subject = $_POST[''];
-$mailFrom = $_POST[''];
-$message = $_POST[''];
-
-$mailto = "MAIL-RECEIVING-EMAIL";
+$subject = $_POST['Web Inquiry'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message'];
+$company = $_POST['company'];
+$mailto = "m.sandoval@maccor.com";
 $headers = "From: ".$mailFrom;
 $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
-
+$txt = $_POST['address1'];
 mail($mailto, $subject, $txt, $headers);
 header("Location: AFTER-SEND-PAGE?mailsend");
 }
