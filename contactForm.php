8f585f3feeada2eff5e819f5ac31fdc7b2e7e3af
MikeSando <34343247+MikeSando@users.noreply.github.com>
2024-09-06 14:28:24 -0500

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -5,11 +5,11 @@ $name = $_POST['name'];
 $subject = $_POST['Web Inquiry'];
 $mailFrom = $_POST['email'];
 $message = $_POST['message'];
-$company = $_POST['company'];
+
 $mailto = "m.sandoval@maccor.com";
 $headers = "From: ".$mailFrom;
 $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
-$txt = $_POST['address1'];
+
 mail($mailto, $subject, $txt, $headers);
 header("https://maccor-web-prototype-3wgyp.ondigitalocean.app/thankyou.html");
 }
