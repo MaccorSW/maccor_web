29e644cf527fd5ad244e2bc7b2eba5533692d35d
MikeSando <34343247+MikeSando@users.noreply.github.com>
2024-10-24 15:17:15 -0500

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -1,15 +1,36 @@
-<?php
+<?php 
+if(isset($_POST['submit'])){
+    $to = "m.sandoval@maccor.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-if (isset($_POST['submit'])) {
-$name = $_POST['name'];
-$subject = $_POST['Web Inquiry'];
-$mailFrom = $_POST['email'];
-$message = $_POST['message'];
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender -->
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
 
-$mailto = "m.sandoval@maccor.com";
-$headers = "From: ".$mailFrom;
-$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+<!DOCTYPE html>
+<head>
+<title>Form submission</title>
+</head>
+<body>
 
-mail($mailto, $subject, $txt, $headers);
-header("https://maccor-web-prototype-3wgyp.ondigitalocean.app/thankyou.html");
-}
+<form action="" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html> 
